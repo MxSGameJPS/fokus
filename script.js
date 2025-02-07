31027ed9d72b92eaf7d36d80c1296ee26ab47b29
MxSGameJPS <mxsgamejps@gmail.com>
2025-02-07 06:38:26 -0300

    finalizando

---
+++ script.js
@@ -0,0 +1,120 @@
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const banner = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+const startPauseBt = document.querySelector("#start-pause");
+const musicaFocoInput = document.querySelector("#alternar-musica");
+const iniciarOuPausarBt = document.querySelector("#start-pause span");
+const iniciarOuPausarBtIcon = document.querySelector(
+  ".app__card-primary-butto-icon"
+);
+const tempoNaTela = document.querySelector("#timer");
+const audio = new Audio("/sons/luna-rise-part-one.mp3");
+const audioPlay = new Audio("/sons/play.wav");
+const audioPause = new Audio("/sons/pause.mp3");
+const audioTempoFinalizado = new Audio("/sons/beep.mp3");
+
+let tempoDecorridoEmSegundo = 1500;
+let intervaloId = null;
+audio.loop = true;
+
+musicaFocoInput.addEventListener("change", () => {
+  if (audio.paused) {
+    audio.play();
+  } else {
+    audio.pause();
+  }
+});
+
+focoBt.addEventListener("click", () => {
+  tempoDecorridoEmSegundo = 1500;
+  alterarContexto("foco");
+  focoBt.classList.add("active");
+});
+
+curtoBt.addEventListener("click", () => {
+  tempoDecorridoEmSegundo = 300;
+  alterarContexto("descanso-curto");
+  curtoBt.classList.add("active"); // Substitua 'descanso-curto' pelo nome do contexto desejado.  //
+});
+
+longoBt.addEventListener("click", () => {
+  tempoDecorridoEmSegundo = 900;
+  alterarContexto("descanso-longo");
+  longoBt.classList.add("active"); // Substitua 'descanso-longo' pelo nome do contexto desejado.  //
+});
+
+function alterarContexto(contexto) {
+  mostrarTempo();
+  botoes.forEach((botao) => botao.classList.remove("active"));
+  titulo.innerHTML = ""; // Substitua '' pelo conteúdo desejado.  //
+  html.setAttribute("data-contexto", contexto);
+  banner.setAttribute("src", `/imagens/${contexto}.png`);
+  switch (contexto) {
+    case "foco":
+      titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `;
+      break;
+    case "descanso-curto":
+      titulo.innerHTML = `
+            Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `;
+      break;
+    case "descanso-longo":
+      titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `;
+    default:
+      break;
+  }
+}
+
+const contagemRegressiva = () => {
+  if (tempoDecorridoEmSegundo <= 0) {
+    audioTempoFinalizado.play();
+    alert("Tempo descanso finalizado!");
+    zerar();
+    return;
+  }
+  tempoDecorridoEmSegundo -= 1;
+  mostrarTempo();
+};
+
+startPauseBt.addEventListener("click", iniciarOuPausar);
+
+function iniciarOuPausar() {
+  if (intervaloId) {
+    audioPause.play();
+    zerar();
+    return;
+  }
+  audioPlay.play();
+  intervaloId = setInterval(contagemRegressiva, 1000);
+  iniciarOuPausarBt.innerHTML = "Pausar";
+  iniciarOuPausarBtIcon.setAttribute("src", "/imagens/pause.png");
+}
+
+function zerar() {
+  clearInterval(intervaloId);
+  iniciarOuPausarBt.textContent = "Começar";
+  iniciarOuPausarBtIcon.setAttribute("src", "/imagens/play_arrow.png");
+  intervaloId = null;
+}
+
+function mostrarTempo() {
+  const tempo = new Date(tempoDecorridoEmSegundo * 1000);
+  const tempoFormatado = tempo.toLocaleTimeString("pt-Br", {
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  tempoNaTela.innerHTML = `${tempoFormatado}`;
+}
+
+mostrarTempo();
